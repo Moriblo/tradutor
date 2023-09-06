ddfb0b20156e0e8415b4e9f8f33f9a5b84674811
Moacyr Ribeiro Blondet <blondet.mr@gmail.com>
2023-09-06 19:36:51 -0300

    Primeira entrega para produção

--- tradutor.py
+++ tradutor.py
@@ -11,31 +11,46 @@ import os, sys, json
 
 from logger import setup_logger
 
-# Inicialize service_name com o nome exclusivo do serviço para fins de geração de
-# arquivo de log
+# ==============================================================================
+""" Inicializa service_name com o nome exclusivo do serviço para fins de geração
+    de arquivo de log.
+"""
+# ==============================================================================
 service_name = "tradutor"
 logger = setup_logger(service_name)
 
-# Informações da API
+# ==============================================================================
+""" Informações de identificação, acesso e documentação do serviço
+"""
+# ==============================================================================
 info = Info(title="Traduz Obra", version="1.0.0")
 app = OpenAPI(__name__, info=info)
 
-# Configurações de "Cross-Origin Resource Sharing"
+# ==============================================================================
+""" Configurações de "Cross-Origin Resource Sharing"
+"""
+# ==============================================================================
 # Foi colocado "supports_credentials=False" para evitar possíveis conflitos com
-# algum tipo de configuração de browser a ser utilizado pelos professores. Mas
-# não é a melhor recomendação por segurança. Para melhorar a segurança desta API,
-# o mais indicado segue nas linhas 30 a 34.
+# algum tipo de configuração de browser. Mas não é a melhor recomendação por 
+# segurança. Para melhorar a segurança desta API, o mais indicado segue nas 
+# linhas abaixo comentadas.
 CORS(app, supports_credentials=False)
 
-#origins_permitidas = ["http://exemplo:3000", "https://meu-site.com"]
+# origins_permitidas = ["Obras de Arte"]
 # Configurando o CORS com suporte a credenciais
-#CORS(app, origins=origins_permitidas, supports_credentials=True)
-#CORS(app, supports_credentials=True, expose_headers=["Authorization"])
+# CORS(app, origins=origins_permitidas, supports_credentials=True)
+# CORS(app, supports_credentials=True, expose_headers=["Authorization"])
 # Adicionalmente utilizar da biblioteca PyJWT
 
+# ==============================================================================
+""" Rota /tradutor para tratar o fetch de `GET` do script.js.
+"""
+# ==============================================================================
 @app.route('/tradutor', methods=['GET'])
 def tradutor():
-    origin = request.headers.get("Origin")
+    # Lê identificação da origem da solicitação de uso desta API
+    origin = request.headers.get('X-Origin')
+    
     # Lê o valor do parâmetro de consulta 'entrada' da solicitação
     entrada = request.args.get('entrada')
 
